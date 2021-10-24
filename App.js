91dded6cdfe270e12acf581b31f03f38c3637bcb
UIT.19522048 <74783259+uit19522048@users.noreply.github.com>
2021-10-24 20:42:48 +0700

    Add files via upload

--- App.js
+++ App.js
@@ -1,14 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Login from './Screen/Login';
+import Welcome from './Screen/Welcome';
+import Profile from './Screen/Profile';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const Stack = createNativeStackNavigator();
+
+export default class App extends Component {
+  render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          {/* <Stack.Screen
+            name="Login"
+            component={Login}
+          /> */}
+          <Stack.Screen
+            name="Welcome"
+            component={Welcome}
+            options={{
+              headerLeft: () => <Text></Text>,
+              headerTitle: () => <Text>Welcome</Text>
+            }}
+          />
+          {/* <Stack.Screen
+            name="Profile"
+            component={Profile}
+          /> */}
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -18,4 +42,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  Logo: {
+    width: 50,
+    height: 50
+  }
 });
